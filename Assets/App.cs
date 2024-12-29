da11844ea4d7dd7e80790da5a9f6fd7a06866cbd
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-12-29 12:08:55 +0700

    Add first code
    Add first code

---
+++ Assets/App.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void Btn_start(){
+        
+    }
+
+    public void Btn_exit(){
+        Application.Quit();
+    }
+}
